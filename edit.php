2428900c140d2652eeb220a756b79cfcbb98638d
Karoliina Roots <karoliina.roots@gmail.com>
2016-03-31 13:13:12 +0300

    valmis

--- edit.php
+++ edit.php
@@ -8,13 +8,53 @@
 	
 		//redirect user
 		echo"redirect";
+		
 		header("Location: table.php");
 		exit(); //dont execute further
+		
 			}else{	
 		echo "User want to edit row:".$_GET["edit"];
 		
 		
 		$mysql = new mysqli("localhost", $db_username, $db_password, "webpr2016_karoliinar");
+		
+		
+		//maye user wants to update data after clicking the button
+		if(isset($_GET["to"]) && isset($_GET["message"])){
+		
+			echo "User modified data, tries to save";
+			
+			//should be validataion?
+			
+			$stmt = $mysql->prepare("UPDATE messages_sample SET recipient=?, message=? WHERE id=?");
+			
+			echo $mysql->error; 
+			
+			$stmt->bind_param("ssi", $_GET["to"], $_GET["message"], $_GET["edit"]);
+			
+			if($stmt->execute()){
+			
+				echo "saved successfully";
+				
+				//option one- redirect
+				header("Location: table.php");
+				exit();
+				
+				//option two - update variables 
+				//$recipient = $_GET["to"];
+				//$message = $_GET["message"];
+				//$id= $_GET["edit"];
+				
+			}else{
+			
+				echo $stmt->error;
+			}
+			
+		}
+		
+		
+		
+		
 		
 		$stmt = $mysql->prepare("SELECT id, recipient, message FROM messages_sample WHERE id=?");
 		
@@ -48,6 +88,8 @@
 <h2> First application </h2>
 
 <form method="get">
+
+	<input hidden name="edit" value="<?=$id;?>">
 
 	<input name="edit" value="<?=$id;?>"><br></br>
 	<label for="to">to:* </label>
